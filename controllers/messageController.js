92058f707b1f64fe6ef12ed37ae3ef093b1b3171
aditiyadwiramadani88 <aditiyadwiramadani88@gmail.com>
2024-09-18 13:38:14 +0700

    refactor: improve status message and media message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,48 +1,51 @@
 const { Device, User } = require('../models');
 const { HandleWhastApp, Qrcode } = require('../utils/whatsAppHandler/connectionWa');
-const { sendMessageValidated } = require('../validation/sendMessage');
+const { sendMessageValidated, sendMediaMessageValidated } = require('../validation/sendMessage');
 
 
 const sendMessage = async (req, res) => {
+    console.log(req.params.id)
     const { message, phone } = req.body
     const { error } = sendMessageValidated.validate({ message, phone })
     if (error) {
         return res.status(400).json({ error: error.details[0].message })
     }
     try {
-        const device = Device.findByPk(req.params.id)
+        const device = await Device.findByPk(req.params.id)
         if (!device) {
             return res.status(400).json({ error: "Device not found!" });
         }
-        if (!device.status) {
+        if (!device.dataValues.status) {
             return res.status(400).json({ error: "Device is disconnected" });
         }
         const handler = new HandleWhastApp(req.params.id)
-        await handler.message({ phone, text: message })
+        await handler.message({ text: message, phone })
         return res.json({ message: "Success sending message" });
-    } catch (errr) {
+    } catch (err) {
+        console.log(err)    
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
 const sendMessageMedia = async (req, res) => {
     const { caption, phone, fileUrl, mime } = req.body
-    const { error } = sendMessageValidated.validate({ caption, phone, fileUrl, mime  })
+    const { error } = sendMediaMessageValidated.validate({ caption, phone, fileUrl, mime  })
     if (error) {
         return res.status(400).json({ error: error.details[0].message })
     }
     try {
-        const device = Device.findByPk(req.params.id)
+        const device = await Device.findByPk(req.params.id)
         if (!device) {
             return res.status(400).json({ error: "Device not found!" });
         }
-        if (!device.status) {
+        if (!device.dataValues.status) {
             return res.status(400).json({ error: "Device is disconnected" });
         }
         const handler = new HandleWhastApp(req.params.id)
-        await handler.media({ phone, caption, mime })
+        await handler.media({ phone, caption, mime, url: fileUrl })
         return res.json({ message: "Success sending media message" });
-    } catch (errr) {
+    } catch (err) {
+        console.log(err)
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 
